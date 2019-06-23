f8e3e1ea073e4e06cea0e184c0c031163ed0da8a
Jorge Cabanas <jorge.cabanas@mercadolibre.com>
2019-06-23 01:38:00 -0300

    use intl in App component

--- admin/client/App/index.js
+++ admin/client/App/index.js
@@ -10,6 +10,11 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { addLocaleData, IntlProvider } from 'react-intl';
+
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+import translation from '../translation';
 
 import App from './App';
 import Home from './screens/Home';
@@ -17,6 +22,8 @@ import Item from './screens/Item';
 import List from './screens/List';
 
 import store from './store';
+
+addLocaleData([...en, ...es]);
 
 // Sync the browser history to the Redux store
 const history = syncHistoryWithStore(browserHistory, store);
@@ -26,14 +33,16 @@ import { listsByKey } from '../utils/lists';
 Keystone.User = listsByKey[Keystone.userList];
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router history={history}>
-			<Route path={Keystone.adminPath} component={App}>
-				<IndexRoute component={Home} />
-				<Route path=":listId" component={List} />
-				<Route path=":listId/:itemId" component={Item} />
-			</Route>
-		</Router>
-	</Provider>,
+	<IntlProvider locale={'es'} messages={translation.es}>
+		<Provider store={store}>
+			<Router history={history}>
+				<Route path={Keystone.adminPath} component={App}>
+					<IndexRoute component={Home} />
+					<Route path=":listId" component={List} />
+					<Route path=":listId/:itemId" component={Item} />
+				</Route>
+			</Router>
+		</Provider>
+	</IntlProvider>,
 	document.getElementById('react-root')
 );
